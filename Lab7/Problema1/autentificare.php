3e31f6b43896b0dfe2c7843d692b84fb46f41177
Andrei Moldovan <andreimoldovan6@gmail.com>
2022-12-03 16:49:29 +0200

    Lab7

--- Lab7/Problema1/autentificare.php
+++ Lab7/Problema1/autentificare.php
@@ -3,7 +3,7 @@ session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'magazin';
+$DATABASE_NAME = 'magazin2';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER,
     $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
